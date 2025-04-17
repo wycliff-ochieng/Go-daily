7d8499e5e85c2d9b06f9722881d33a92183ac213
wycliff <wyckieochieng91@gmail.com>
2025-04-17 21:46:36 +0300

    ->

---
+++ strarting/pointers.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+//when to use ponters
+// 1. updating state
+type User struct {
+	name   string
+	email  string
+	number int32
+}
+
+func (u User) Email() string {
+	return u.email
+}
+func Email(user User) string {
+	return user.email
+}
+
+func (u User) updateEmail(email string) string {
+	u.email = email
+	return email
+}
+
+func (u *User) updateEmailAgain(email string) string {
+	u.email = email
+	return email
+}
+
+func main() {
+	fmt.Println("pointers .... ")
+	user := User{
+		email: "fooobar@g.com",
+	}
+	user.updateEmail("aggg@bar.com")
+	user.updateEmailAgain("yoo@bitch.com")
+	fmt.Println(user.Email())
+
+}
