d85e2ada0bf9718f1c4bfa43c687a05002e53237
wycliff <wyckieochieng91@gmail.com>
2025-04-21 03:41:50 +0300

    next

--- projects/server.go
+++ projects/server.go
@@ -4,14 +4,34 @@ import (
 	//"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 )
+func main() {
+	http.HandleFunc("/hello", myServer)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+	mux
+}
+
+//handler : myServer( function that processes https request in a web server)
+// w - response writer. ResponseWriter is an interface used by http handler handle http requests
+// req - http ,method whih deals with all 
 
 func myServer(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, " Hello, first sever\n")
 }
 
-func main() {
-	http.HandleFunc("/hello", myServer)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+// serveMux - basic httpRouter in golang
+// multipelxing - is able to handle many routes
+
+type customServeMux struct{}
+
+func (p customServeMux) ServeHttp(w http.ResponseWriter, r *http.Request){
+	if r.URL
 }
+
+func giveRandom(w http.ResponseWriter, r *http.Request){
+	fmt.Pritnln(w, "your random numer is %f", rand.Float32())
+}
+
+
